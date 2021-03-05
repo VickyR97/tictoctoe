80c9227caf0a42de2e2415a748f1d51be6917f9e
VickyR97 <97vickywaran@gmail.com>
2021-03-05 12:48:11 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,125 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="outer">
+    <div class="container d-flex flex-column justify-content-center align-items-center">
+      <h1 class="mt-5 text-white">Tic toc toe Game</h1>
+      <div class="mt-5">
+          <h2 
+          v-if="isCross"
+          class="text-center mb-5 text-danger">Cross turn</h2>
+          <h2 
+          v-if="!isCross"
+          class="text-center mb-5 text-primary">Circle turn</h2>
+
+            <div class="grid">
+                <div v-for="(element, index) in boxes"
+                :key="index"
+                >
+                    <Icon
+                    :iconName="element"
+                    @click="handleClick(index)"
+                    />
+                </div>
+            </div> 
+            <div class="d-flex justify-content-center mt-4">
+              <button
+              @click="reset"
+              class="btn btn-lg btn-danger fw-bold">Restart game</button>
+            </div>        
+      </div>
+      
+    </div>    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Icon from "./components/icon";
+import Alert from "sweetalert2/dist/sweetalert2.js"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Icon
+  },
+  data(){
+    return{
+      isCross: false,
+      winMessage: '',
+      boxes: new Array(9).fill('empty')
+    }
+  },
+  watch:{
+    winMessage: function(message){
+      if(message){
+        this.showDialog()
+      }
+    }
+  },
+  methods:{
+    handleClick(index){
+      if(this.winMessage){
+        return this.showDialog()
+      }
+      if(this.boxes[index] === "empty"){
+        this.boxes[index] = this.isCross ? "cross" : "circle" 
+        this.isCross = !this.isCross
+      }else{
+        return Alert.fire("Already filled...")
+      }
+      this.checkWinner()
+    },
+    reset(){
+      this.winMessage = ""
+      this.boxes = new Array(9).fill("empty")
+      this.isCross = false
+    },
+    checkWinner(){
+      if(this.boxes[0] === this.boxes[1] && this.boxes[0] === this.boxes[2] && this.boxes[0] !== "empty"){
+        this.winMessage = `${this.boxes[0]} is Won`
+      }
+      else if(this.boxes[3] === this.boxes[4] && this.boxes[3] === this.boxes[5] && this.boxes[3] !== "empty"){
+        this.winMessage = `${this.boxes[0]} is Won`
+      }
+      else if(this.boxes[6] === this.boxes[7] && this.boxes[6] === this.boxes[8] && this.boxes[6] !== "empty"){
+        this.winMessage = `${this.boxes[0]} is Won`
+      }
+      else if(this.boxes[0] === this.boxes[3] && this.boxes[0] === this.boxes[6] && this.boxes[0] !== "empty"){
+        this.winMessage = `${this.boxes[0]} is Won`
+      }
+      else if(this.boxes[1] === this.boxes[4] && this.boxes[1] === this.boxes[7] && this.boxes[1] !== "empty"){
+        this.winMessage = `${this.boxes[0]} is Won`
+      }
+      else if(this.boxes[2] === this.boxes[5] && this.boxes[2] === this.boxes[8] && this.boxes[2] !== "empty"){
+        this.winMessage = `${this.boxes[0]} is Won`
+      }
+      else if(this.boxes[0] === this.boxes[4] && this.boxes[0] === this.boxes[8] && this.boxes[0] !== "empty"){
+        this.winMessage = `${this.boxes[0]} is Won`
+      }
+      else if(this.boxes[2] === this.boxes[4] && this.boxes[2] === this.boxes[6] && this.boxes[2] !== "empty"){
+        this.winMessage = `${this.boxes[0]} is Won`
+      }
+      else if(this.boxes.includes("empty") !== true){  
+        this.reset()
+        return Alert.fire("Match Draw ")
+        
+      }
+    },
+    showDialog(){
+      Alert.fire({
+        title: "Game Over",
+        text: `${this.winMessage}`
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.outer{
+  background-color: rgb(53, 52, 52);
+  height: 100vh;
+}
+.grid{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 5px;
 }
 </style>
---
+++ src/components/icon.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="icon-outer d-flex justify-content-center align-items-center rounded">
+      <i 
+      v-if="iconName == 'circle'"
+      class="far fa-circle fa-4x text-primary"></i>
+      <i 
+      v-if="iconName == 'cross'"
+      class="fas fa-times fa-4x text-danger"></i>
+      <i
+      v-if="iconName == 'empty'"
+      ></i>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        iconName:{
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.icon-outer{
+    height: 100px;
+    width: 100px;
+    background-color: white;
+}
+</style>
